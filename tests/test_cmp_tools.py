be4336969463f621fb10d0c5142c6ac7fee94eac
Darren <darren.autonomous@gmail.com>
2025-12-03 15:31:50 +0700

    CHORE (tests): add more tests

--- tests/test_cmp_tools.py
+++ tests/test_cmp_tools.py
@@ -33,10 +33,43 @@ async def test_stand_down():
         result = await client.call_tool("stand_down", {})
         print(result)
 
+async def test_go_ahead():
+    client = Client(
+        transport=StreamableHttpTransport(url=BASE_URL)
+    )
+    async with client:
+        assert client.is_connected()
+        result = await client.call_tool("go_ahead", {})
+        print(result)
+
+
+async def test_turn_left():
+    client = Client(
+        transport=StreamableHttpTransport(url=BASE_URL)
+    )
+    async with client:
+        assert client.is_connected()
+        result = await client.call_tool("turn_left", {})
+        print(result)
+
+async def test_turn_right():
+    client = Client(
+        transport=StreamableHttpTransport(url=BASE_URL)
+    )
+    async with client:
+        assert client.is_connected()
+        result = await client.call_tool("turn_right", {})
+        print(result)
+
+
+
 async def test_all_cmp_tools():
     await test_list_tools()
     await test_stand_up()
     await test_stand_down()
-
+    await test_go_ahead()
+    await test_turn_left()
+    await test_turn_right()
+    
 if __name__ == "__main__":
     asyncio.run(test_all_cmp_tools())
